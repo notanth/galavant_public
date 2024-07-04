aea0020bc6252b14ee05cd504536421bfa611fba
Bob Belderbos <bobbelderbos@gmail.com>
2024-07-04 18:26:00 +0200

    from coding session

--- galavant/galavant/settings.py
+++ galavant/galavant/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'galavant.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["galavant/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
